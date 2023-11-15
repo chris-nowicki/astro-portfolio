37f82a4f5fa66fa7c31673695bba0c62b88b8608
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-11-15 00:04:06 -0500

    Fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,28 @@
 // load environment variables from .env file
-import { loadEnv } from 'vite'
-const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
-  import.meta.env.MODE,
-  process.cwd(),
-  ''
-)
+import { loadEnv } from 'vite';
+const {
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET
+} = loadEnv(import.meta.env.MODE, process.cwd(), '');
+const projectId = PUBLIC_SANITY_PROJECT_ID;
+const dataset = PUBLIC_SANITY_DATASET;
 
-const projectId = PUBLIC_SANITY_PROJECT_ID
-const dataset = PUBLIC_SANITY_DATASET
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
 
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
-import react from '@astrojs/react'
-import sanity from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapters: vercel(),
-  integrations: [
-    sanity({
-      projectId,
-      dataset,
-      studioBasePath: '/admin',
-      useCdn: false,
-      apiVersion: '2023-03-20',
-    }),
-    react(),
-    tailwind(),
-  ],
-})
+  adapter: vercel()
+  integrations: [sanity({
+    projectId,
+    dataset,
+    studioBasePath: '/admin',
+    useCdn: false,
+    apiVersion: '2023-03-20'
+  }), react(), tailwind()],
+});
