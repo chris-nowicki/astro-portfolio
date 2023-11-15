66cb9c5d447f66050d2e0eeb461fcc138f0d2192
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-11-14 23:27:26 -0500

    Embed Sanity Studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
+import sanity from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  output: 'hybrid',
+  adapters: vercel(),
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: '4zd5l3k5',
+      dataset: 'production',
+      useCdn: false,
+      studioBasePath: '/admin',
+    }),
+    react(),
+  ],
+})
